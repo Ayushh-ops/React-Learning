2e3fa79ca4d2a2f80765179a7cdbd9113df4f645
Ayushh-ops <asinha30012004@gmail.com>
2026-08-06 22:35:49 +0530

    Initial React project

---
+++ 01basicreact/src/App.jsx
@@ -0,0 +1,14 @@
+import Test from "./test"
+
+function App() {
+
+  return (
+    <>
+  <Test/>
+  <h1>Can write this while using fragment 'tag'</h1>
+  <p>Can return only one element but use fragment and can add many more element </p>
+  </>
+  )
+}
+
+export default App
