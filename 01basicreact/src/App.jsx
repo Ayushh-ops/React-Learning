edd6278c39a67b1a9356a9c908435b6e060a0d8d
Ayushh-ops <asinha30012004@gmail.com>
2026-08-07 01:25:16 +0530

    Created react Library

--- 01basicreact/src/App.jsx
+++ 01basicreact/src/App.jsx
@@ -1,11 +1,11 @@
 import Test from "./test"
 
 function App() {
-
+    const username="hello"
   return (
     <>
   <Test/>
-  <h1>Can write this while using fragment 'tag'</h1>
+  <h1>Can write this while using fragment 'tag' {username}</h1>
   <p>Can return only one element but use fragment and can add many more element </p>
   </>
   )
